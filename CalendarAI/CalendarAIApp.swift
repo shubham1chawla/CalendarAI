5a9433302390e705b6ece4f18fe4770f989956ca
Shubham Chawla <shubham_chawla@live.com>
2023-11-22 02:12:18 -0700

    Initial commit

---
+++ CalendarAI/CalendarAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAIApp.swift
+//  CalendarAI
+//
+//  Created by Shubham Chawla on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
