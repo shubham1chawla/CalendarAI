48275abe5e3af68cf9d14222d4f99f3848f9447d
Aashna Budhiraja <aashna.budhiraja.ab@gmail.com>
2023-11-30 12:43:38 -0700

    adding readme

---
+++ CalendarAI/CalendarAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAIApp.swift
+//  CalendarAI
+//
+//  Created by Aashna Budhiraja on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeatherBaseView()
+        }
+    }
+}
---
+++ CalendarAI/README.md
@@ -0,0 +1,6 @@
+README
+
+Instructions to run the Weather Module:
+
+1. Generate your API key for Open Weather Map: https://home.openweathermap.org/api_keys
+2. Update the key in Services > WeatherService, line 11.
