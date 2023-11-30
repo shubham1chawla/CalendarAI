7ed4db45683778b004d6de79e1d519c1091a3aba
Jai Narula <72813144+jainarula@users.noreply.github.com>
2023-11-30 01:03:44 -0700

    Calender Service

--- CalendarAI/Views/CalendarView.swift
+++ CalendarAI/Views/CalendarView.swift
@@ -13,7 +13,26 @@ struct CalendarView: View {
     private var store = EKEventStore()
     @State var events: [EKEvent] = []
     
+    init() {
+        checkPermissions()
+    }
+    
+    private func checkPermissions() {
+        let status = EKEventStore.authorizationStatus(for: .event)
+        switch (status) {
+        case .notDetermined:
+            store.requestFullAccessToEvents { granted, error in
+                
+            }
+        case .fullAccess:
+            break
+        default:
+            print("No access! Status: \(status)")
+        }
+    }
+    
     var body: some View {
+//        Text("Calender Events")
         List {
             ForEach(events, id: \.self) { event in
                 VStack(alignment: .leading) {
@@ -38,7 +57,15 @@ struct CalendarView: View {
         }
         .listStyle(.plain)
         .onAppear {
-            self.events = [];
+            let startDate = Date()
+            let endDate = Date(timeIntervalSinceNow: 30 * 24 * 3600)
+            let calendars = store.calendars(for: .event)
+            let predicate = store.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
+            var events: [EKEvent] = []
+            for event in store.events(matching: predicate) {
+                events.append(event)
+            }
+            self.events = events;
         }
     }
     
