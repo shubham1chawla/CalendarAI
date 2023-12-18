338a081ad572ef1b0f13b6a87d914de6b21a6eb0
Shubham Chawla <shubham_chawla@live.com>
2023-12-19 00:58:55 +0530

    used core data for symptoms & intensities

---
+++ CalendarAI/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CalendarAI
+//
+//  Created by Shubham Chawla on 12/18/23.
+//
+
+import Foundation
+
+struct Keys {
+    static let APPLICATION_NAME = "CalendarAI"
+    static let LAST_USER_SESSSION = "lastUserSession"
+}
